68683fa87561f6c88c6c67666bb9ff1b626cfa3a
manturra <46898761+manturra@users.noreply.github.com>
2019-03-16 13:28:36 +0100

    Update cliente_chat.py

--- cliente_chat.py
+++ cliente_chat.py
@@ -1,19 +1,20 @@
 import socket
 
 puerto = 8080
-ip = "10.108.33.6"
- def chat(cliente):
-     condicion = True
-     while condicion:
-         mensaje_cliente = input("Comienzo del chat")
-         send_bytes = str.encode(mensaje_cliente)
-         cliente.send(send_bytes)
-         mensaje_servidor = cliente.recv(2048).decode("utf-8")
-         print(mensaje_servidor)
+ip = "10.0.2.15"
+def chat(cliente):
+    condicion = True
+    while condicion:
+        mensaje_cliente = input("")
+        mensaje_cliente = str.encode(mensaje_cliente)
+        cliente.send(mensaje_cliente)
+        mensaje_servidor = cliente.recv(2048).decode("utf-8")
+        print(mensaje_servidor)
+        
+cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 try:
-    cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     cliente.connect((ip, puerto))
     chat(cliente)
-except OError:
-    print("Socket está usándose.")
-    cliente.close
+except KeyboardInterrupt:
+    cliente.close()
+    print("La conversación se acabó.")
